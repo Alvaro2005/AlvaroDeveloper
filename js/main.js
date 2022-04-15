dccc1bbaca096bb7a84cc45c45de30a8af6c6e52
Alvaro2005 <78221033+Alvaro2005@users.noreply.github.com>
2022-04-15 13:00:57 -0400

    Primer diseño del home

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const type = new Typed('.typed', {
+    strings: ['Hello, I m Alvaro patiño,front end developer, take a look at my portfolio'],
+    // stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+    typeSpeed: 50,
+    startDelay: 50,
+    backSpeed: 20,
+});
